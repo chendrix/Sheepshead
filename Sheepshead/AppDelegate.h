291828481b1043328f929d4e934505dc129f2049
Chris Hendrix <cah276@cornell.edu>
2015-05-21 23:31:22 -0700

    Initial Commit

---
+++ Sheepshead/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sheepshead
+//
+//  Created by Christopher Hendrix on 5/21/15.
+//  Copyright (c) 2015 Christopher Hendrix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
